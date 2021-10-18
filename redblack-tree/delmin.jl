d50bd28df1334ea6439301726ab760edb6b7dd26
Tarak Shah <tarak@hrdag.org>
2021-10-18 09:50:16 -0700

    delmin knows all argument types rather than just container

--- redblack-tree/delmin.jl
+++ redblack-tree/delmin.jl
@@ -1,44 +1,41 @@
-is_red(::E) = false
-is_red(tree::Red) = true
-is_red(tree::Black) = false
+# vim: set ts=4 sts=0 sw=4 si fenc=utf-8 et:
+# vim: set fdm=marker fmr={{{,}}} fdl=0 foldcolumn=4:
 
-function delmin(t::Red{T}) where {T}
-    is_empty(t.left) && return t.right
-    if is_empty(t.left.left) && is_red(t.left.right)
-        return Red(t.key,
-                   Black(t.left.right.key, E{T}(), E{T}()),
-                   t.right)
-    elseif is_empty(t.left.left) && is_empty(t.right.left)
-        b = Red(t.key, E{T}(), E{T}())
-        return Black(t.right.key, b, t.right.right)
-    elseif is_empty(t.left.left) && is_red(t.right.left)
-        b = Black(t.key, t.left.right, E{T}())
-        d = Black(t.right.key, E{T}(), t.right.right)
-        return Red(t.right.left.key, b, d)
-    elseif is_red(t.left.left)
-        a = delmin(t.left.left)
-        b = Black(t.left.key, a, t.left.right)
-        return Red(t.key, b, t.right)
-    elseif is_red(t.right.left) && !is_red(t.left.right)
-        a = delmin(Red(t.left.key,
-                       turn_black(t.left.left),
-                       turn_black(t.left.right)))
-        b = Black(t.key, a, t.right.left.left)
-        d = Black(t.right.key, t.right.left.right, t.right.right)
-        return Red(t.right.left.key, b, d)
-    elseif !is_red(t.left.right)
-        b = delmin(Red(t.left.key, t.left.left, t.left.right))
-        if is_red(b) return Red(t.key, turn_black(b), t.right) end
-        d = Red(t.key, b, t.right.left)
-        return Black(t.right.key, d, t.right.right)
+function delmin(key::T, left::RB{T}, right::RB{T}) where {T}
+    is_empty(left) && return right
+    if is_empty(left.left) && is_red(left.right)
+        return Red(key,
+                   Black(left.right.key, E{T}(), E{T}()),
+                   right)
+    elseif is_empty(left.left) && is_empty(right.left)
+        b = Red(key, E{T}(), E{T}())
+        return Black(right.key, b, right.right)
+    elseif is_empty(left.left) && is_red(right.left)
+        b = Black(key, left.right, E{T}())
+        d = Black(right.key, E{T}(), right.right)
+        return Red(right.left.key, b, d)
+    elseif is_red(left.left)
+        a = delmin(left.left.key, left.left.left, left.left.right)
+        b = Black(left.key, a, left.right)
+        return Red(key, b, right)
+    elseif is_red(right.left) && !is_red(left.right)
+        a = delmin(left.key, turn_black(left.left), turn_black(left.right))
+        b = Black(key, a, right.left.left)
+        d = Black(right.key, right.left.right, right.right)
+        return Red(right.left.key, b, d)
+    elseif !is_red(left.right)
+        b = delmin(left.key, left.left, left.right)
+        if is_red(b) return Red(key, turn_black(b), right) end
+        d = Red(key, b, right.left)
+        return Black(right.key, d, right.right)
     else
-        b = delmin(Red(t.left.key,
-                       t.left.left,
-                       t.left.right.left))
-        d = Black(t.left.right.key,
+        b = delmin(left.key,
+                    left.left,
+                    left.right.left)
+        d = Black(left.right.key,
                   b,
-                  t.left.right.right)
-        return Red(t.key, d, t.right)
+                  left.right.right)
+        return Red(key, d, right)
     end
 end
 
@@ -48,15 +45,14 @@ function delete_min(t::Black{T}) where {T}
         return turn_black(t.right)
     end
 
-    is_red(t.left) && return Black(t.key, delmin(t.left), t.right)
+    is_red(t.left) && return Black(t.key, delmin(t.left.key, t.left.left, t.left.right), t.right)
 
     if !is_red(t.right)
-        res = delmin(Red(t.key, t.left, t.right))
+        res = delmin(t.key, t.left, t.right)
         return turn_black(res)
     end
 
-    b = Red(t.key, t.left, t.right.left)
     return Black(t.right.key,
-                 delmin(b),
+                 delmin(t.key, t.left, t.right.left),
                  t.right.right)
 end
