dcfd15f1067b2b200ae971f290e7388ac937fb9f
Tarak Shah <tarak@hrdag.org>
2021-10-18 09:41:21 -0700

    optimize contains to cap number of compares, ex 2.2 in purely functional data structures

--- redblack-tree/contains.jl
+++ redblack-tree/contains.jl
@@ -1,6 +1,11 @@
 Base.contains(::E{T}, ::T) where {T} = false
+Base.contains(::E{T}, key::T, sentinel::T) where {T} = isequal(key, sentinel)
 function Base.contains(tree::RB{T}, key::T) where {T}
     key <  tree.key && return contains(tree.left, key)
-    key >  tree.key && return contains(tree.right, key)
-    return true
+    contains(tree.right, key, tree.key)
 end
+function Base.contains(tree::RB{T}, key::T, sentinel::T) where {T}
+    key <  tree.key && return contains(tree.left, key, sentinel)
+    contains(tree.right, key, tree.key)
+end
+
